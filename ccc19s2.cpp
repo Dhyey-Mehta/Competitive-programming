9a45115ccc0e9983ebe04da4df12f7cdcc24ed9f
daythekid <37392632+daythekid@users.noreply.github.com>
2020-05-21 15:57:13 -0400

    Made it work

--- ccc19s2.cpp
+++ ccc19s2.cpp
@@ -1,21 +1,38 @@
-#include <iostream>
-#include <math>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 
 bool isprime(int num){
-	for (int iter=2;iter<sqrt(n);iter++){
+	for (int iter=2;iter<=sqrt(num);iter++){
 		if (num%iter == 0){
 			return false;
 		}
-	return true;
 	}
+	return true;
 }
 
 
 int main(){
-	cout<<isprime(2)
-	
-	
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int numCases;
+	cin>>numCases;
+
+	for(int w=0;w<numCases;w++){
+		int target;
+		cin>>target;
+		target *=2;
+		if (isprime(target-2)){
+			cout<<"2"<<" "<<target-2<<endl;
+			continue;
+		}
+		for (int w=3;w<=target/2;w+=2){
+			if (isprime(target-w)&&isprime(w)){
+				cout<<w<<" "<<target-w<<endl;
+				break;
+			}
+		}
+
+	}
 }
