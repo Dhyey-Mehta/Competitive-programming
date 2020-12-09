230bcb850647d8e39e727821ea585066510bc8df
daythekid <37392632+daythekid@users.noreply.github.com>
2020-12-08 20:28:40 -0500

    Create ccc12j3.py

---
+++ ccc12j3.py
@@ -0,0 +1,10 @@
+num = int(input())
+
+for i in range(num):
+    print("*"*num + "x"*num + "*"* num)
+
+for i in range(num):
+    print(" "*num+"x"*num+"x"*num)
+
+for i in range(num):
+    print("*"*num+" "*num+"*"*num)
